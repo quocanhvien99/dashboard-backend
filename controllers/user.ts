df4d0e2115fad9fcf039db95340bc5b8401d4dd7
quocanhv99 <quocanhv99@gmail.com>
2022-03-26 10:54:00 +0700

    add role filter

--- controllers/user.ts
+++ controllers/user.ts
@@ -84,16 +84,27 @@ export function update(req: Request, res: Response) {
 	);
 }
 export function list(req: Request, res: Response) {
-	let { s, skip, limit, orderby, sortby } = req.query;
-	let query = 'select * from user ';
-	if (s) query += `where match(name, email, mobile) against ("${s}")  `;
+	let { s, skip, limit, orderby, sortby, role_id } = req.query;
+	let query =
+		'select role.name as role, user.* from user, role where user.role_id=role.id ';
+	if (s)
+		query += `, match(user.name, user.email, user.mobile) against ("${s}") `;
+	if (role_id) query += `, user.role_id="${role_id}" `;
 	if (limit) query += `limit ${limit} `;
 	if (skip) query += `offset ${skip} `;
-	if (orderby) query += `order by ${orderby} `;
+	if (orderby) query += `order by user.${orderby} `;
 	if (sortby) query += `${sortby} `;
 
 	const sqlCon = req.app.locals.sqlCon;
 	sqlCon.query(query, (err: any, result: any) => {
+		if (err) return res.status(400).json(err);
+		res.json(result);
+	});
+}
+export function getuser(req: Request, res: Response) {
+	const { id } = req.params;
+	const sqlCon = req.app.locals.sqlCon;
+	sqlCon.query(`select * from user where id`, (err: any, result: any) => {
 		if (err) return res.status(400).json(err);
 		res.json(result);
 	});
